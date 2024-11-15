fb98733782fe9e7301517320cabc3cee90c4b0b9
mangesh0910 <mangeshgosavi300@gmail.com>
2024-11-15 13:06:01 +0530

    Added enums

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import Navbar from "./scenes/navbar"
+import { SelectedPage } from "./shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>('Home');
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return (
     <div className="app bg-gray-20">
