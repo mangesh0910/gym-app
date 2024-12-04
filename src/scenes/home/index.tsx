a7bbac482a24197fc727530f3ce1563c6437464f
mangesh0910 <mangeshgosavi300@gmail.com>
2024-12-04 16:10:11 +0530

    Added animation

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorFortune from '@/assets/SponsorFortune.png';
 import SponsorRedBull from '@/assets/SponsorRedBull.png';
 import ActionButton from '@/shared/ActionButton';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
@@ -25,7 +26,16 @@ const Home = ({ setSelectedPage }: Props) => {
                 {/* HEADING */}
                 <div>
                     {/* ACTUAL HEADING */}
-                    <div>
+                    <motion.div
+                        initial='hidden'
+                        whileInView='visible'
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 }
+                        }}
+                    >
                         <div className='relative z-10'>
                             <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                                 <img alt='home-page-text' src={HomePageText} />
@@ -34,9 +44,19 @@ const Home = ({ setSelectedPage }: Props) => {
                         <p className='py-8'>
                             Gold's Gym is a globally renowned fitness brand that has made its mark in India. With a strong legacy dating back to 1965 in Venice Beach, California, Gold's Gym has become synonymous with fitness excellence and innovation.
                         </p>
-                    </div>
+                    </motion.div>
                     {/* ACTIONS */}
-                    <div className='flex gap-16 items-center'>
+                    <motion.div
+                        className='flex gap-16 items-center'
+                        initial='hidden'
+                        whileInView='visible'
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 }
+                        }}
+                    >
                         <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
                         <AnchorLink
                             className='text-sm underline text-primary-500 hover:text-secondary-500'
@@ -45,7 +65,7 @@ const Home = ({ setSelectedPage }: Props) => {
                         >
                             Learn more
                         </AnchorLink>
-                    </div>
+                    </motion.div>
                 </div>
                 {/* IMAGE */}
                 <div>
